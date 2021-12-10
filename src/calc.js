b0eb1113e08a75375e9844660d799895f0ebd96d
mohankumar <mohankumar.vs@codilar.com>
2021-12-10 11:43:15 +0530

    calculator app

---
+++ src/calc.js
@@ -0,0 +1,10 @@
+// let res=document.getElementById("result");
+// function dis(val)
+// {
+//     console.log(val);
+// document.getElementById("result").value+=val;
+//  }
+ dis=(val)=>{
+    console.log(val);
+    document.getElementById("result").value += val;
+}
